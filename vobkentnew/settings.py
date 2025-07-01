8381fbd56428085651add575ffc1ad54767cb134
Ravshan047 <ravshanraxmatullayev34@gmail.com>
2025-07-01 13:51:49 +0500

    vobsinov

--- vobkentnew/settings.py
+++ vobkentnew/settings.py
@@ -130,6 +130,9 @@ import dj_database_url
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
--- vobkentnew/urls.py
+++ vobkentnew/urls.py
@@ -43,6 +43,10 @@ urlpatterns = [
 ]
 
 # Media fayllar uchun (rasmlar, fayllar)
+from django.conf import settings
+from django.conf.urls.static import static
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
